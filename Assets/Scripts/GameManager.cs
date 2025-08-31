57556b488e68639cccf94c82cd7b184012056e87
ElieCookie13 <eleanor@appcharge.com>
2025-08-31 23:01:13 +0300

    wip

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,54 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
+    public Player player;
+    public Text scoreText;
+    public GameObject gameOverScreen;
+    public GameObject playButton;
     private int score = 0;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+        gameOverScreen.SetActive(false);
+        PauseGame();
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false;
+    }
+    public void StartGame()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+
+        gameOverScreen.SetActive(false);
+        playButton.SetActive(false);
+
+        Time.timeScale = 1f;
+        player.enabled = true;
+
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+
+        for (int i = 0; i < pipes.Length; i++)
+        {
+            Destroy(pipes[i].gameObject);
+        }
+    }
 
     public void IncreaseScore()
     {
         score++;
+        scoreText.text = score.ToString();
     }
     
     public void Gameover()
     {
-        Debug.Log("Game Over! Your score: " + score);
+        gameOverScreen.SetActive(true);
+        playButton.SetActive(true);
+        PauseGame();
     }
 }
